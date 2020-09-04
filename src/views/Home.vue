2a2077dff707ca93ae93817dc904b648f97e77fc
Mathias Ciarlo Thorstensen <mathiast@netcompany.com>
2020-09-04 20:24:54 +0200

    Initial stuff 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <FrontPage />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import FrontPage from "@/components/FrontPage";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    FrontPage
   }
 };
 </script>
